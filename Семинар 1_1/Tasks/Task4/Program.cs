d2852984fcaec47ed2fa2136c91770889063c243
Elbrus5642 <satellitprof@gmail.com>
2022-12-09 11:31:26 +0300

    Семинар 09.12.22

---
+++ Семинар 1_1/Tasks/Task4/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Напишите программу, 
+которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+//получение числа с консоли
+//получение числа с консоли
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        /*
+        2 типа данных: reference types(ссылочные) и value types(значимые)
+        */
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1 && result <=4)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число");
+        }
+    }
+
+    return result;
+
+}
+
+string GetQurter (int number)
+{
+    if (number ==1)
+    {
+      return "Первая четверть x > 0, y > 0";
+    }
+    if (number == 2)
+    {
+      return "Первая четверть x < 0, y > 0";
+    }
+if (number == 3)
+    {
+      return "Первая четверть x < 0, y < 0";
+    }
+    
+      return "Первая четверть x > 0, y < 0";
+  
+}
+
+int number = GetNumber ("Введите номер четверти: ");
+string quater = GetQurter (number); 
+Console.WriteLine(quater);
---
+++ Семинар 1_1/Tasks/Task7/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 24: Напишите программу,
+ которая принимает на вход число (А)
+  и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 
+*/
+
+// Метод, который паодлучает число с консоли
+int GetNumber(string message)
+
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+                if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число");
+        }
+    }
+
+    return result;
+
+}
+
+int number = GetNumber("Введите число N:"); 
+// Метод, который считает сумму от 1 до числа N
+
+ int GetSum(int num)
+{
+    int sum = 0; 
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+  
+}
+int sumNumber = GetSum(number);
+Console.WriteLine($"{sumNumber}");
